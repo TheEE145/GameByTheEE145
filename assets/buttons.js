88acf18498942037fb8b1a08dcd45ca349a55608
TheEE145 <72571816+TheEE145@users.noreply.github.com>
2021-11-29 17:46:46 +0200

    modal step 100%

--- assets/buttons.js
+++ assets/buttons.js
@@ -1,1 +1,7 @@
-//no code here
+document.querySelector(".modal-btn1").addEventListener("click", function() {
+    document.querySelector(".modal-1").setAttribute("data-open", "1")
+})
+
+document.querySelector(".close1").addEventListener("click", function() {
+    document.querySelector(".modal-1").setAttribute("data-open", "0")
+})
